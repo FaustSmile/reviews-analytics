18d92edc12c7a464d2488a9461be8bee529507c8
FaustSmile <s56142@yahoo.com.tw>
2022-05-01 14:58:37 +0800

    add word count function

--- read.py
+++ read.py
@@ -8,10 +8,12 @@ with open("reviews.txt", "r") as f:
             print(len(data))
 print("檔案讀取完成，總共有", len(data), "筆資料")
 
+
 sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
 print("留言平均長度為", sum_len/len(data))
+
 
 new = []
 for d in data:
@@ -19,4 +21,42 @@ for d in data:
         new.append(d)
 print("一共有", len(new), "筆留言長度小於100") 
 print(new[0])
-print(new[1])   
+print(new[1])
+
+
+good = []
+for d in data:
+    if "good" in d:
+        good.append(d)
+print("一共有", len(good), "筆留言提到GOOD")
+
+good = [d for d in data if "good" in d]
+
+
+# 文字的記數
+wc = {} #word_count
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1 # 新增新的KEY 進 wc 字典
+
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+print(len(wc))
+
+while True:
+    word = input('請輸入您想查詢的字:')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為:', wc[word])
+    else:
+        print('這個字沒有出現過喔!')
+
+print('謝謝您的使用!!')
+
